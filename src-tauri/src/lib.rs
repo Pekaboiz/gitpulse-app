d638e4a2613b5bf3c50eed9786095444d1b19363
peki4 <mr.mr.riki@list.ru>
2026-05-10 01:30:05 +0500

    Minor fix: extra empty lines are created during logging

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -254,24 +254,24 @@ struct HistoryItem {
     created_at : String,
 }
 
-fn add_history_item(app : &tauri::AppHandle, hist_item : HistoryItem) -> Result<(), String> {
-    let file_path = repositories_file_path(&app, "logger")?;
+fn add_history_item(app: &tauri::AppHandle, hist_item: HistoryItem) -> Result<(), String> {
+    let file_path = repositories_file_path(app, "logger")?;
 
-    let mut item : Vec<HistoryItem> = if file_path.exists() {
+    let mut items: Vec<HistoryItem> = if file_path.exists() {
         let content: String = std::fs::read_to_string(&file_path)
-        .map_err(|error| error.to_string())?;
+            .map_err(|error| error.to_string())?;
 
         serde_json::from_str(&content)
             .map_err(|error| error.to_string())?
     } else {
-        vec![HistoryItem::default()]
+        Vec::new()
     };
 
-    item.push(hist_item);
+    items.push(hist_item);
 
-    let json = serde_json::to_string_pretty(&item)
-                    .map_err(|error| error.to_string())?;
-    
+    let json = serde_json::to_string_pretty(&items)
+        .map_err(|error| error.to_string())?;
+
     std::fs::write(&file_path, json)
         .map_err(|error| error.to_string())?;
 
